9420bbee91a49fbb732ae166bba63b53eafd5411
MartinStefanec <martin.stefanec@azet.sk>
2017-06-05 14:17:58 +0200

    oprava #1

--- analyzator1.c
+++ analyzator1.c
@@ -8,95 +8,117 @@
 #include<sys/stat.h>
 #include<unistd.h>
 
-
-	
 main(int argc, char *argv[]){
-	int subor,velkost,n,presiel=0,i,k,j,pocetfloatov,medzipocet=0,kolko;
+	int subor,velkost,n,presiel,i=0,k,j,pocetfloatov=0,medzipocet=0,kolko;
 	unsigned char cislo;
-	float number,sucet,priemer;	
+	float number,sucet,priemer,*pole,*priemery;	
+	int *dlzky;
+	
+	presiel=0;
 	if(argc!=2){
 		printf("Expected 1 argument");
 		return 0;
 	}
+	
 	subor=open(argv[1], O_RDONLY|O_BINARY,S_IWUSR);
 	if(subor<0) {
 		printf("Wrong filename, Try again");
 		return 0;
-	}	
+	}
+		
 	velkost = lseek(subor, 0, SEEK_END);
 	lseek(subor, 0, SEEK_SET);
-	int dlzky[velkost/40];
 	while(presiel<velkost){
 		if(read(subor,&cislo,sizeof(unsigned char))==-1) {
 			printf("Unable to read");
 			return 0;
 		}
+		
 		lseek(subor,sizeof(float)*cislo,SEEK_CUR);
-		dlzky[n]=cislo;
 		n++;
 		presiel=presiel+sizeof(float)*cislo+1;
-}
-	printf("Pocet postupnosti: %d\n",n);
-	for(i=0;i<n;i++) {
-	pocetfloatov=pocetfloatov+dlzky[i];
 	}
 	
-printf("Celkovy pocet floatov: %d\n",pocetfloatov);
+	printf("Pocet postupnosti: %d\n",n);
 	lseek(subor, 0, SEEK_SET);
-	float *pole[pocetfloatov];
-	for(i=0;i<pocetfloatov;i++){
-		pole[i]=malloc(sizeof(float));
-		if(pole[i]==0) {
-			printf("Unable to allocate");
-			return 0;
-		}
+	dlzky=malloc(n*sizeof(int));
+	if(dlzky==NULL) {
+		printf("Unable to allocate");
+		return 0;
 	}
-	for(i=0;i<n;i++){
-		lseek(subor, 1, SEEK_CUR);
-		 medzipocet=medzipocet+dlzky[i-1];
-		for(j=0;j<dlzky[i];j++){
-			if(read(subor,&number,sizeof(float))==-1) {
+			
+	for(i=0;i<n;i++) {
+		if(read(subor,&cislo,sizeof(unsigned char))==-1) {
 			printf("Unable to read");
 			return 0;
 		}
-				kolko=medzipocet+j;
-				*pole[kolko]=number;
+		
+		lseek(subor,sizeof(float)*cislo,SEEK_CUR);
+		dlzky[i]=cislo;
+		pocetfloatov=pocetfloatov+dlzky[i];
+	}
+	
+	printf("Celkovy pocet floatov: %d\n",pocetfloatov);
+	lseek(subor, 0, SEEK_SET);
+	pole=malloc(pocetfloatov*sizeof(float));
+	if(pole==NULL) {
+		printf("Unable to allocate");
+		return 0;
+	}
+	
+	for(i=0;i<n;i++){
+		lseek(subor, 1, SEEK_CUR);
+		for(j=0;j<dlzky[i];j++){
+			if(read(subor,&number,sizeof(float))==-1) {
+				printf("Unable to read");
+				return 0;
 			}
+			
+			kolko=medzipocet+j;
+			pole[kolko]=number;
+		}
+		
+		medzipocet=medzipocet+dlzky[i];
 	}
 	
 	medzipocet=0;
-	float priemery[n];
+	priemery=malloc(n*sizeof(float));
 	printf("Priemery jednotlivych postupnosti:\n");
 	for(i=0;i<n;i++){
 		sucet=0;
-		medzipocet=medzipocet+dlzky[i-1];
 		for(j=medzipocet;j<medzipocet+dlzky[i];j++){
-			sucet=sucet+*pole[j];
+			sucet=sucet+pole[j];
 		}
+		
+		medzipocet=medzipocet+dlzky[i];	
 		priemery[i]=sucet/dlzky[i];
 		printf("%d:%f\t",i+1,priemery[i]);
 	}
+	
 	sucet=0;
 	medzipocet=0;	
 	printf("\nPriemer priemerov postupnosti: ");
 	for(i=0;i<n;i++){
 		sucet=sucet+priemery[i];
 	}
+	
 	priemer=sucet/n;
 	printf("%f\n",priemer);
 	printf("Jednotlive postupnosti:");
 	for(i=0;i<n;i++){
-		printf("\n%d. postupnost: ",i+1);
-		medzipocet=medzipocet+dlzky[i-1];
+		printf("\n%d. postupnost: ",i+1);	
 		for(j=medzipocet;j<medzipocet+dlzky[i];j++){
-			printf("%f, ",*pole[j]);
+			printf("%f, ",pole[j]);
 		}
+		
+		medzipocet=medzipocet+dlzky[i];
 	}
-	for(i=0;i<pocetfloatov;i++)	free(pole[i]);
 	
+	free(pole);
+	free(priemery);
+	free(dlzky);
 	if(close(subor)==-1) {
-			printf("Unable to close file");
-			return 0;
-		}
-	
+		printf("Unable to close file");
+		return 0;
+	}
 }
